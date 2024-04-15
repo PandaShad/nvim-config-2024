335e0ee4a6192928ad90874c09ac410fcc4538eb
pandashad <maximebellet83@hotmail.fr>
2024-04-15 22:32:44 +0200

    update nvim config

--- lua/lilshad/plugins/lsp/mason.lua
+++ lua/lilshad/plugins/lsp/mason.lua
@@ -2,11 +2,14 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local mason = require("mason")
 
     local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup({
       ui = {
@@ -30,6 +33,17 @@ return {
         "emmet_ls",
         "prismals",
         "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
+        "pylint",
+        "eslint_d",
       },
     })
   end,
