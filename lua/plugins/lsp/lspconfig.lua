d3f9326572fd4e91352ac4f5b696a899631cbd05
Maxime Bellet <maxime.bellet@wit.fr>
2025-05-13 12:17:00 +0200

    update mason and lspconfig

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -72,5 +72,98 @@ return {
 			underline = true, -- Specify Underline diagnostics
 			update_in_insert = false, -- Keep diagnostics active in insert mode
 		})
+
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
+		-- emmet_ls
+		lspconfig.emmet_ls.setup({
+			capabilities = capabilities,
+			filetypes = {
+				"html",
+				"typescriptreact",
+				"javascriptreact",
+				"css",
+
+				"sass",
+				"scss",
+				"less",
+				"svelte",
+			},
+		})
+
+		-- emmet_language_server
+		lspconfig.emmet_language_server.setup({
+			capabilities = capabilities,
+
+			filetypes = {
+				"css",
+				"eruby",
+				"html",
+				"javascript",
+				"javascriptreact",
+
+				"less",
+				"sass",
+				"scss",
+				"pug",
+				"typescriptreact",
+			},
+			init_options = {
+				includeLanguages = {},
+				excludeLanguages = {},
+				extensionsPath = {},
+
+				preferences = {},
+				showAbbreviationSuggestions = true,
+				showExpandedAbbreviation = "always",
+				showSuggestionsAsSnippets = false,
+				syntaxProfiles = {},
+				variables = {},
+			},
+		})
+
+		-- denols
+		lspconfig.denols.setup({
+			capabilities = capabilities,
+			root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+		})
+
+		-- ts_ls (replaces tsserver)
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+			root_dir = function(fname)
+				local util = lspconfig.util
+				return not util.root_pattern("deno.json", "deno.jsonc")(fname)
+					and util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")(fname)
+			end,
+			single_file_support = false,
+			init_options = {
+				preferences = {
+					includeCompletionsWithSnippetText = true,
+					includeCompletionsForImportStatements = true,
+				},
+			},
+		})
 	end,
 }
