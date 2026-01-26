d14407dd24c0f4a4654105e18d7e0837e226cfdf
Maxime Bellet <maxime.bellet@wit.fr>
2026-01-26 16:08:27 +0100

    update: nvim-conf

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -222,8 +222,8 @@ return {
 		-- })
 
 		-- Add other LSP servers as needed, e.g., gopls, eslint, html, etc.
-		-- lspconfig.gopls.setup({ capabilities = capabilities })
-		-- lspconfig.html.setup({ capabilities = capabilities })
-		-- lspconfig.cssls.setup({ capabilities = capabilities })
+		lspconfig.gopls.setup({ capabilities = capabilities })
+		lspconfig.html.setup({ capabilities = capabilities })
+		lspconfig.cssls.setup({ capabilities = capabilities })
 	end,
 }
